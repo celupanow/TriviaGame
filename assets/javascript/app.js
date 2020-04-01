12ded77207b85709524b165dcc2bff1e45fdf372
Christina Lupanow <celupanow@gmail.com>
2020-04-01 13:09:51 -0600

    updating files, adding images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,38 @@
+
+//declaring variables
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
-var seconds = 20;
+var seconds = 120;
 var clockRunning = false;
 var intervalId;
 
+//function timer if the seconds are greater than zero, it decrements seconds and displays the timer
+//else it stops the clock and displays the results
 function timer() {
-    seconds--;
-    $("#timer").html(seconds);
+
+    if (seconds > 0){
+        seconds--;
+        $("#timer").html(seconds);
+    } else {
+        clearInterval(intervalId);
+        clockRunning = false;
+        results();
+    }
+
 }
 
+//start function starts the timer and sets the clock to running
+//calls the timer function
 function start() {
     if (!clockRunning) {
       intervalId = setInterval(timer, 1000);
       clockRunning = true;
     }
-
-    if (seconds === 0) {
-        clearInterval(intervalId);
-        clockRunning = false;
-    }
 }
 
-    // function stop() {
-        
-    //   }
-
+//results function determines how many answers were correct, incorrect, or unanswered
+//displays the results
 function results() {
     if (!$("input[name=q1]:checked").val()) {
         unanswered++;
@@ -90,40 +97,30 @@ function results() {
     $("#form").html("<p>Correct Answers: " + correctAnswers + "</p><br><p>Incorrect Answers: " + incorrectAnswers + "</p><br><p>Unanswered Questions: " + unanswered);
 }
 
-
+//this hides the quiz and the timer
 $("#form").hide();
+$("#timeLeft").hide();
 
 //onclick event to start the game
-
 $("#btn").on("click", function(){
 
+    //hides the start button and shows the quiz and timer
     $("#content").hide();
     $("#form").show();
+    $("#timeLeft").show();
 
+    //starts the timer
     start();
-
-
 });
 
 
-
+//onclick event for the done button
 $("#done").on("click", function(){
 
-  results();
-
+    //run the results function
+    results();
+    //stops the clock
+    clearInterval(intervalId);
+    clockRunning = false;
+    $("#timeLeft").hide();
 });
-
-//set timer for 120 seconds & start it
-
-//listen for the radio button values
-//$("input[type='button']").click(function(){
-    // var radioValue = $("input[name='gender']:checked").val();
-    // if(radioValue){
-    //     alert("Your are a - " + radioValue);
-
-
-//keep track of how many correct/incorrect answers
-
-//on click for done
-
-//display correct/incorrect/unanswered when timer runs out or when done button is clicked
